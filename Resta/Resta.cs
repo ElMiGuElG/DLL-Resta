e6f8aa699c5a735be1f5cfa8af7c40b1eef7242f
Usuario <Usuario@B13-205-22879>
2022-09-13 11:28:05 -0500

    Agregar archivos de proyecto.

---
+++ Resta/Resta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NachoOpera_Resta
+{
+    public class Resta
+    {
+        /// <summary>
+        /// Metodo Resta
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="B"></param>
+        /// <returns></returns>
+        /// 
+        public static long Re_Resta(long A, long B)
+        {
+            return ( A - B);
+        }
+    }
+}
